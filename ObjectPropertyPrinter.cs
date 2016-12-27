8dbc3bf0197772e56543fe935345cb7b79cba326
mardyu <michealyxd@hotmail.com>
2016-12-27 23:32:02 +0800

    - add feature and comment

--- ObjectPropertyPrinter.cs
+++ ObjectPropertyPrinter.cs
@@ -1,4 +1,4 @@
-
+﻿
 /*
 * ObjectPropertyPrinter.cs: print all property in object via reflect as json format
 * ***
@@ -18,7 +18,11 @@
 *		- Dictionary<K, Dictionary<K, V>>
 *		- Dictionary<K, Dictionary<K, Dictionary<K, V>>>
 *		- List<Dictionary<K, V>>
-*
+*	6. if class has IEnumerable interface or others inherit from IEnumerable will have a array taged with ArrayValues(may same as others) as extend part, 
+*		if you donot want it, comment USE_EXTEND_PART definer
+*	7. can print property in struct, but has some limit, only print **direct** info about property of struct.
+*	8. **well known bugs**: return new same class/struct instance in property of the class/struct which will cause infinite loop util StringBuiler
+*		run out of your memory!
 *
 * Authors:
 * mardyu<michealyxd@hotmail.com>
@@ -26,6 +30,8 @@
 * Copyright 2016 mardyu<michealyxd@hotmail.com>
 * Licensed under the MIT license. See LICENSE file in the project root for full license information.
 */
+
+#define USE_EXTEND_PART
 
 using System.Collections;
 using System.Collections.Generic;
@@ -45,6 +51,13 @@ namespace Mard.Tools
 		private const string cPrintTemplateWithArray = ", \"{0}\": [";
 		private const string cPrintTemplateValue = "\"{0}\"";
 		private const string cPrintTemplateValueNotQuote = "{0}";
+		private const string cPrintEmpty = "{}";
+
+		private const string cCustomValueNameInClass = "ArrayValues";
+
+		private const int cIteratorCountInfinite = 2;
+		private const int cIteratorCountLastOne = 1;
+		private const int cIteratorCountNone = 0;
 
 		private static readonly HashSet<Type> rPrimaryType = new HashSet<Type>()
 		{
@@ -89,7 +102,7 @@ namespace Mard.Tools
 			return !rPrimaryTypeWithoutQuote.Contains(_t);
 		}
 
-		private static void FormatProperty(StringBuilder _sb, PropertyInfo _pi, System.Object _o, HashSet<object> _alreadyUsed)
+		private static void FormatProperty(StringBuilder _sb, PropertyInfo _pi, System.Object _o, HashSet<object> _alreadyUsed, int iterlevel = cIteratorCountInfinite)
 		{
 			if (IsBaseType(_pi.PropertyType))
 			{
@@ -98,11 +111,11 @@ namespace Mard.Tools
 			else
 			{
 				_sb.AppendFormat(cPrintTemplateWithStructure, _pi.Name);
-				_sb.Append(PrintProperty(_pi.PropertyType, _o, _alreadyUsed));
+				_sb.Append (PrintProperty (_pi.PropertyType, _o, _alreadyUsed, iterlevel));
 			}
 		}
 
-		private static void FormatProperty(StringBuilder _sb, FieldInfo _fi, System.Object _o, HashSet<object> _alreadyUsed)
+		private static void FormatProperty(StringBuilder _sb, FieldInfo _fi, System.Object _o, HashSet<object> _alreadyUsed, int iterlevel = cIteratorCountInfinite)
 		{
 			if (IsBaseType(_fi.FieldType))
 			{
@@ -111,11 +124,11 @@ namespace Mard.Tools
 			else
 			{
 				_sb.AppendFormat(cPrintTemplateWithStructure, _fi.Name);
-				_sb.Append(PrintProperty(_fi.FieldType, _o, _alreadyUsed));
+				_sb.Append (PrintProperty (_fi.FieldType, _o, _alreadyUsed, iterlevel));
 			}
 		}
 
-		private static void FormatType(StringBuilder _sb, Type _t, System.Object _o, HashSet<object> _alreadyUsed)
+		private static void FormatType(StringBuilder _sb, Type _t, System.Object _o, HashSet<object> _alreadyUsed, int iterlevel = cIteratorCountInfinite)
 		{
 			if (IsBaseType(_t))
 			{
@@ -123,7 +136,7 @@ namespace Mard.Tools
 			}
 			else
 			{
-				_sb.Append(PrintProperty(_t, _o, _alreadyUsed));
+				_sb.Append(PrintProperty(_t, _o, _alreadyUsed, iterlevel));
 			}
 		}
 
@@ -240,10 +253,10 @@ namespace Mard.Tools
 			return PrintProperty(_t, _o, null);
 		}
 
-		public static string PrintProperty(Type _t, System.Object _o, HashSet<object> _alreadyUsed)
+		public static string PrintProperty(Type _t, System.Object _o, HashSet<object> _alreadyUsed, int iterlevel = cIteratorCountInfinite)
 		{
-			if (null == _o || null == _t)
-				return "{}";
+			if (null == _o || null == _t || iterlevel == cIteratorCountNone)
+				return cPrintEmpty;
 
 			if (IsBaseType(_t)) 
 			{
@@ -275,43 +288,65 @@ namespace Mard.Tools
 				sb.AppendFormat("\"class_name\":\"{0}\"", GetRightClassName(_o.GetType()));
 			}
 			_alreadyUsed.Add(_o);
-			if (!_t.IsValueType)
+#if USE_EXTEND_PART
+			List<Type> o_interfaces = new List<Type> (_t.GetInterfaces ());
+			if (null != _o && o_interfaces.Contains(rIteratorType))
 			{
-				foreach (var pi in pis)
+				sb.AppendFormat(cPrintTemplateWithArray, cCustomValueNameInClass);
+				foreach (var it in (IEnumerable)_o)
 				{
-					if (pi.GetIndexParameters().Length > 0 || IsObsolete(pi))
-						continue;
-					var value = pi.GetValue(_o, null);
-					//_alreadyUsed.Add(value);
-					if (pi.PropertyType.IsGenericType)
-					{
-						List<Type> interfaces = new List<Type>(pi.PropertyType.GetInterfaces());
-						if (null != value && interfaces.Contains(rIteratorType))
-						{
-							sb.AppendFormat(cPrintTemplateWithArray, pi.Name);
-							foreach (var it in (IEnumerable)value)
-							{
-								FormatType(sb, it.GetType(), it, _alreadyUsed);
-								sb.Append(',');
-							}
-							if (sb[sb.Length - 1] == ',')
-								sb.Length -= 1;
-							sb.Append("]");
-						}
-						else if (null != value)
-						{
-							sb.AppendFormat(cPrintTemplateNotQuote, pi.Name, PrintProperty(pi.PropertyType, value, _alreadyUsed));
-						}
-						else
-							sb.AppendFormat(cPrintTemplateNotQuote, pi.Name, "null");
-					}
-					else
-					{
-						FormatProperty(sb, pi, value, _alreadyUsed);
-					}
-					//_alreadyUsed.Remove(value);
+					FormatType(sb, it.GetType(), it, _alreadyUsed);
+					sb.Append(',');
+				}
+				if (sb[sb.Length - 1] == ',')
+					sb.Length -= 1;
+				sb.Append("]");
+			}
+#endif
+			if (_t.IsValueType) {
+				switch (iterlevel) {
+				case cIteratorCountInfinite:
+					iterlevel = cIteratorCountLastOne;
+					break;
+				case cIteratorCountLastOne:
+				default:
+					iterlevel = cIteratorCountNone;
+					break;
 				}
 			}
+			foreach (var pi in pis)
+			{
+				if (pi.GetIndexParameters().Length > 0 || IsObsolete(pi))
+					continue;
+				var value = pi.GetValue(_o, null);
+				if (pi.PropertyType.IsGenericType)
+				{
+					List<Type> interfaces = new List<Type>(pi.PropertyType.GetInterfaces());
+					if (null != value && interfaces.Contains(rIteratorType))
+					{
+						sb.AppendFormat(cPrintTemplateWithArray, pi.Name);
+						foreach (var it in (IEnumerable)value)
+						{
+							FormatType(sb, it.GetType(), it, _alreadyUsed, iterlevel);
+							sb.Append(',');
+						}
+						if (sb[sb.Length - 1] == ',')
+							sb.Length -= 1;
+						sb.Append("]");
+					}
+					else if (null != value)
+					{
+						sb.AppendFormat(cPrintTemplateNotQuote, pi.Name, PrintProperty(pi.PropertyType, value, _alreadyUsed, iterlevel));
+					}
+					else
+						sb.AppendFormat(cPrintTemplateNotQuote, pi.Name, "null");
+				}
+				else
+				{
+					FormatProperty(sb, pi, value, _alreadyUsed, iterlevel);
+				}
+			}
+
 			FieldInfo[] fis = _t.GetFields();
 			foreach (var fi in fis)
 			{
